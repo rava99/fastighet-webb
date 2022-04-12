fb06456d1528485b91c00ade7da1bb55ad3021c1
DavidR99 <David.ravanelli2@gmail.com>
2022-04-12 15:49:46 +0200

    Bootstrap template + kod fastighet

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import MainPageIndex from './MainPageIndex'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <MainPageIndex />
   </React.StrictMode>,
   document.getElementById('root')
 );
